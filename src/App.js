fbdd6b5ad49b96320dadda97c8a86def017ce401
alTabWBC <wd.costa@campus.fct.unl.pt>
2021-01-30 22:50:34 +0000

    Webgl1

--- src/App.js
+++ src/App.js
@@ -4,69 +4,66 @@ import Recorder from "./recorder";
 import React, { Component } from "react";
 import { v4 } from "uuid";
 
-const unityContent = new UnityContent(
-  "Build/game.json",
-  "Build/UnityLoader.js"
-);
+const unityContent = new UnityContent("Build/webgl1/game.json", "Build/webgl1/UnityLoader.js");
 
 class App extends Component {
-  generateUUID = () => {
-    const startIndex = Math.round(Math.random() * 10);
-    const id = v4().substring(startIndex, startIndex + 6);
-    return id;
-  };
-
-  constructor(props) {
-    super(props);
-    let id = localStorage.getItem("id");
-    if (id === null) {
-      id = this.generateUUID();
-      localStorage.setItem("id", id);
-    }
-    this.state = {
-      id: id,
-      gameId: 0,
+    generateUUID = () => {
+        const startIndex = Math.round(Math.random() * 10);
+        const id = v4().substring(startIndex, startIndex + 6);
+        return id;
     };
-  }
 
-  avaliateProduction = (message) => {
-    const response = JSON.parse(message);
-    if (parseInt(response["gameId"]) !== this.state.gameId) return;
-    if (response["response"].toLowerCase() === "true") {
-      console.log("characterMoves");
-      unityContent.send("Character", "moveCharacter", "True");
+    constructor(props) {
+        super(props);
+        let id = localStorage.getItem("id");
+        if (id === null) {
+            id = this.generateUUID();
+            localStorage.setItem("id", id);
+        }
+        this.state = {
+            id: id,
+            gameId: 0,
+        };
     }
-  };
 
-  onClick = () => {
-    unityContent.send("Character", "moveCharacter", "True");
-  };
+    avaliateProduction = (message) => {
+        const response = JSON.parse(message);
+        if (parseInt(response["gameId"]) !== this.state.gameId) return;
+        if (response["response"].toLowerCase() === "true") {
+            console.log("characterMoves");
+            unityContent.send("Character", "moveCharacter", "True");
+        }
+    };
 
-  increaseId = () => {
-    this.setState({ gameId: this.state.gameId + 1 });
-  };
+    onClick = () => {
+        unityContent.send("Character", "moveCharacter", "True");
+    };
 
-  render() {
-    return (
-      <div className={classes.App}>
-        <header className={classes.header}>
-          <div className={classes.Unity}>
-            <Unity unityContent={unityContent} />
-          </div>
-          <div className={classes.centeredDiv}>
-            <Recorder
-              sendMessage={this.avaliateProduction}
-              id={this.state.id}
-              unity={unityContent}
-              gameId={this.state.gameId}
-              newGame={this.increaseId}
-            />
-            <p className={classes.p}>{this.state.id}</p>
-          </div>
-        </header>
-      </div>
-    );
-  }
+    increaseId = () => {
+        this.setState({ gameId: this.state.gameId + 1 });
+    };
+
+    render() {
+        return (
+            <div className={classes.App}>
+                <header className={classes.header}>
+                    <div className={classes.Unity}>
+                        <Unity unityContent={unityContent} />
+                    </div>
+                    <div className={classes.centeredDiv}>
+                        <Recorder
+                            sendMessage={this.avaliateProduction}
+                            id={this.state.id}
+                            unity={unityContent}
+                            gameId={this.state.gameId}
+                            newGame={this.increaseId}
+                        />
+                        <p className={classes.p}>{this.state.id}</p>
+                    </div>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/recorder.js
+++ src/recorder.js
@@ -4,83 +4,84 @@ import RecordButton from "./Record/RecordButton";
 const timeInterval = 900;
 
 const audioConstraints = {
-  channelCount: 1,
-  sampleRate: 44100,
+    channelCount: 1,
+    sampleRate: 44100,
 };
 
 let count = 0;
 
 class Rec extends React.Component {
-  state = {
-    recording: false,
-    label: "",
-  };
+    state = {
+        recording: false,
+        label: "",
+    };
 
-  async componentDidMount() {
-    navigator.mediaDevices
-      .getUserMedia({ audio: audioConstraints })
-      .then((stream) => {
-        this.mediaRecorder = new MediaRecorder(stream);
-        this.mediaRecorder.addEventListener("dataavailable", (event) => {
-          const formData = new FormData();
-          formData.append("file", event.data);
-          fetch("https://biovisualspeech.eu.pythonanywhere.com/postFileWebm/", {
-          // fetch("http://192.168.1.11:5050/postFile/", {
-            headers: {
-              name: event.timecode,
-              segment: count,
-              id: this.props.id,
-              label: this.state.label,
-              gameId: this.props.gameId,
-              "Content-Length": event.data.length,
-            },
-            method: "POST",
-            body: formData,
-          }).then((response) => {
-            if (!response.ok) return;
-            response.text().then((message) => this.props.sendMessage(message));
-          });
+    async componentDidMount() {
+        navigator.mediaDevices.getUserMedia({ audio: audioConstraints }).then((stream) => {
+            this.mediaRecorder = new MediaRecorder(stream);
+            this.mediaRecorder.addEventListener("dataavailable", (event) => {
+                const formData = new FormData();
+                formData.append("file", event.data);
+                fetch("https://biovisualspeech.eu.pythonanywhere.com/postFileWebm/", {
+                    // fetch("http://192.168.1.8:5000/playsound/", {
+                        headers: {
+                            name: event.timecode,
+                            segment: count,
+                            id: this.props.id,
+                            label: this.state.label,
+                            gameId: this.props.gameId,
+                            "Content-Length": event.data.length,
+                        },
+                        method: "POST",
+                        body: formData,
+                    }).then((response) => {
+                        if (!response.ok) return;
+                        response.text().then((message) => this.props.sendMessage(message));
+                    });
+            });
         });
-      });
-    this.props.unity.on("GameOver", () => {
-      this.stopRecording();
-    });
-    this.props.unity.on("GameStart", (message) => {
-      if (message === "Menu") {
-        this.stopRecording();
-        this.setState({ label: "" });
-      } else {
-        const label = message.split("_")[1];
-        this.setState({ label: label });
-        this.props.newGame();
-        this.stopRecording();
-      }
-    });
-  }
+        this.props.unity.on("GameOver", () => {
+            this.stopRecording();
+            this.setState({
+                label: "",
+            });
+        });
+        this.props.unity.on("GameStart", (message) => {
+            if (message === "Menu") {
+                this.stopRecording();
+                this.setState({ label: "" });
+            } else {
+                const label = message.split("_")[1];
+                this.setState({ label: label });
+                this.props.newGame();
+                this.stopRecording();
+            }
+        });
+    }
 
-  startRecording = () => {
-    count = 0;
-    this.mediaRecorder.start(timeInterval);
-    this.setState({ recording: true });
-  };
+    startRecording = () => {
+        count = 0;
+        this.mediaRecorder.start(timeInterval);
+        this.setState({ recording: true });
+    };
 
-  stopRecording = () => {
-    if (this.mediaRecorder.state !== "recording") return;
-    this.mediaRecorder.stop();
-    this.setState({
-      recording: false,
-    });
-  };
+    stopRecording = () => {
+        if (this.mediaRecorder.state !== "recording") return;
+        this.mediaRecorder.stop();
+        this.setState({
+            recording: false,
+        });
+    };
 
-  render() {
-    return this.state.label === "" ? null : (
-      <RecordButton
-        recording={this.state.recording}
-        startRecording={this.startRecording}
-        stopRecording={this.stopRecording}
-      />
-    );
-  }
+    render() {
+        return false && this.state.label === "" ? null : (
+            <RecordButton
+                recording={this.state.recording}
+                startRecording={this.startRecording}
+                stopRecording={this.stopRecording}
+            />
+        );
+    }
 }
 
 export default Rec;
